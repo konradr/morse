1f1285a9fe3246a9a2bd7c4b81a2c9960c14b1e2
Konrad Rzezniczak <konradr@kth.se>
2016-11-03 14:56:20 -0700

    Added reagent

--- src/morse/core.cljs
+++ src/morse/core.cljs
@@ -1,13 +1,19 @@
 (ns morse.core
-  (:require ))
+  (:require [reagent.core :as reagent :refer [atom]]))
 
 (enable-console-print!)
 
-(println "This text is printed from src/morse/core.cljs. Go ahead and edit it and see reloading in action.")
+(println "App started")
 
 ;; define your app data so that it doesn't get over-written on reload
 
 (defonce app-state (atom {:text "Hello world!"}))
+
+(defn hello-world []
+  [:h1 (:text @app-state)])
+
+(reagent/render-component [hello-world]
+                          (. js/document (getElementById "app")))
 
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
