4c25d59bedfd4a3db900cfd261cebc2945d3afeb
Konrad Rzezniczak <konradr@kth.se>
2016-11-03 22:37:43 -0700

    Added initial morse encoding

--- src/morse/core.cljs
+++ src/morse/core.cljs
@@ -1,5 +1,6 @@
 (ns morse.core
-  (:require [reagent.core :as reagent :refer [atom]]))
+  (:require [reagent.core :as reagent :refer [atom]]
+            [morse.code :as morse-code]))
 
 (enable-console-print!)
 
@@ -7,12 +8,39 @@
 
 ;; define your app data so that it doesn't get over-written on reload
 
-(defonce app-state (atom {:text "Hello world!"}))
+(defonce app-state (atom {:text "Hello world!"
+                          :speed 1000}))
 
 (defn hello-world []
-  [:h1 (:text @app-state)])
+  [:p (:text @app-state)])
 
-(reagent/render-component [hello-world]
+(defn atom-input [value]
+  [:textarea {:type "text"
+              :cols 80
+              :rows 10
+           :value @value
+           :on-change #(reset! value (-> % .-target .-value))}])
+
+(def idle "#fff")
+(def on "#f00")
+(defonce color (atom on))
+
+(defn blink [node]
+  )
+
+(defn play [code]
+  )
+
+(defn state []
+  (let [val (atom "Your morse code is here")]
+  (fn []
+    [:div
+     [:div [atom-input val]]
+     [:p "Value: "  (->> @val morse-code/encode (apply str))]
+     #_[:button {:on-click #(play (morse-code/encode @val))} "Play"]
+     #_[:div#play {:style {:width 200 :height 200 :background-color @color}}]])))
+
+(reagent/render-component [state]
                           (. js/document (getElementById "app")))
 
 (defn on-js-reload []
